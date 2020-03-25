3e71288a4dfb386c969c6583bcfc304d1ee97248
Luiz Cunha <luizotcunha@gmail.com>
2020-03-25 17:57:13 -0400

    aprendendo sobre requisiçoes

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (request,response)=>{
-    return response.json({
-        evento: "oii"
-    });
-})
+app.get('/', (request, response) => {
+  return response.json({
+    evento: 'oii',
+  });
+});
+
+app.get('/users', (request, response) => {
+  //Mostra o que ta dentro da url (http://localhost:3333/users?nome=luiz)
+  const params = request.query;
+
+  console.log(params);
+
+  return response.json({
+    mensagem: params,
+  });
+});
+
+app.get('/users/:id', (request, response) => {
+  const params = request.params;
+
+  console.log(params);
+
+  return response.json({
+    Mensagem: params,
+  });
+});
+
+app.post('/users', (request, response) => {
+  const body = request.body;
+
+  console.log(body);
+  return response.json({
+    Mensagens: body,
+  });
+});
 
 app.listen(3333);
