2eb1b6831979632cf4e8879fa7adbb8548245e50
Luiz Cunha <luizotcunha@gmail.com>
2020-03-29 12:31:06 -0400

    modularizando projeto, mvc

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,33 +1,9 @@
 const express = require('express');
 const routes = express.Router();
 
-const connection = require('./database/connection');
+const OngController = require('./controllers/OngController');
 
-const crypto = require('crypto');
-
-routes.get('/ongs', async (request, response) => {
-  const ongs = await connection('ongs').select('*');
-
-  return response.json(ongs);
-});
-
-//Async pois vai esperar realizar a conexão no banco antes de executar o restante dos códigos
-routes.post('/ongs', async (request, response) => {
-  const { name, email, whatsapp, city, uf } = request.body;
-  console.log(name, email, whatsapp, city, uf);
-
-  const id = crypto.randomBytes(4).toString('HEX');
-
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf,
-  });
-
-  return response.json({ id });
-});
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
 module.exports = routes;
