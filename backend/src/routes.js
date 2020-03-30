6439bf4aa1e6bb01b2e1634a1275edefd000d814
Luiz Cunha <luizotcunha@gmail.com>
2020-03-30 14:02:53 -0400

    Inserindo a listagem por ong - Aula 2 1:20

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
@@ -10,5 +11,7 @@ routes.post('/ongs', OngController.create);
 routes.post('/incidents', IncidentController.create);
 routes.get('/incidents', IncidentController.index);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
