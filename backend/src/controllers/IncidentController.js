40c1cee79e4c35232b6eb1a71dbeeda0e0184530
Luiz Cunha <luizotcunha@gmail.com>
2020-03-30 13:38:55 -0400

    Criando rota de delete do incident

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -19,4 +19,24 @@ module.exports = {
     const incidents = await connection('incidents').select('*');
     return response.json(incidents);
   },
+
+  async delete(request, response) {
+    const { id } = request.params;
+    const ong_id = request.headers.authorization;
+
+    const incident = await connection('incidents')
+      .where('id', id)
+      .select('ong_id')
+      .first();
+
+    if (incident.ong_id != ong_id) {
+      return response.status(401).json({ error: 'operação não permitida' });
+    }
+
+    await connection('incidents')
+      .where('id', id)
+      .delete();
+
+    return response.status(204).send();
+  },
 };
