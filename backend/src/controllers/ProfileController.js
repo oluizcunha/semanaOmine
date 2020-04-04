a68250506d5f5d8a071b63487326c60079b3b535
Luiz Cunha <luizotcunha@gmail.com>
2020-04-04 14:56:00 -0400

    Adicionando profile para listagem de caso especifico de apenas uma ong, inserindo backup de insominia no repo

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -1,6 +1,7 @@
 const connection = require('../database/connection');
 
 module.exports = {
+  //retornando casos especificos de uma unica ong ( ficou fora do controler da ong, pq esta tentando seguir documentação de mvc)
   async index(request, response) {
     const ong_id = request.headers.authorization;
 
