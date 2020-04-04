9e5efe1fd893892f3503edfad846675544562cf1
Luiz Cunha <luizotcunha@gmail.com>
2020-04-04 17:36:02 -0400

    Conceito de estado e imutabilidade, podendo mudar só com funcion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 
 function App() {
-  return <Header title="teste2">Filho ou children</Header>;
+  const [counter, setCounter] = useState(0);
+
+  //use State retorna um array com dois valores[valor real, Funcao de atualização]
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div>
+      <Header title="teste2">Filho ou children</Header>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
+  );
 }
 
 export default App;
