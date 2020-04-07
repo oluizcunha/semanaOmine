bba266f9a3ca16abf3b11b12f9d6e0daeabde7a2
Luiz Cunha <luizotcunha@gmail.com>
2020-04-07 16:41:04 -0400

    Estilizando pagina de listagem de profile 01:08

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import './styles.css';
 
@@ -19,6 +19,51 @@ export default function Profile() {
           <FiPower size={18} color="#e02041" />
         </button>
       </header>
+
+      <h1>Casos Cadastrados</h1>
+      <ul>
+        <li>
+          <strong>Caso:</strong>
+          <p>Casoteste</p>
+
+          <strong>Descricao:</strong>
+          <p>DescricaoTeste</p>
+
+          <strong>Valor:</strong>
+          <p>R$Algum</p>
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>Caso:</strong>
+          <p>Casoteste</p>
+
+          <strong>Descricao:</strong>
+          <p>DescricaoTeste</p>
+
+          <strong>Valor:</strong>
+          <p>R$Algum</p>
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>Caso:</strong>
+          <p>Casoteste</p>
+
+          <strong>Descricao:</strong>
+          <p>DescricaoTeste</p>
+
+          <strong>Valor:2</strong>
+          <p>R$Algum</p>
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
